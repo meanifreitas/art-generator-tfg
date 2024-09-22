9c9ae906d0df78672be280d6b278145ee8b1dc83
Meani Freitas <meani.sf@gmail.com>
2024-09-22 15:57:23 -0300

    Moving assets to public dir

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.main`
-  background-image: url('src/assets/background.jpeg');
+  background-image: url('assets/background.jpeg');
   background-size: cover;
   background-position: center;
   height: 90vh;
